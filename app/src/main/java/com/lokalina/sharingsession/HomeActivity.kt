5ba95893ecbbc97e20118d8762bada8cec8a0001
telkomdev-arinal <arinal.rnl@gmail.com>
2019-04-25 19:56:52 +0700

    first commit

---
+++ app/src/main/java/com/lokalina/sharingsession/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.lokalina.sharingsession
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val email = intent.getStringExtra("email")
+        val pass = intent.getStringExtra("pass")
+        val role = intent.getIntExtra("role", 0)
+
+        txEmail.text = "email : $email"
+        txPassword.text = "pass : $pass"
+        txRole.text = "role : $role"
+
+        btnOke.setOnClickListener {
+            setResult(RESULT_OK, Intent().putExtra("data", "Berhasil"))
+            finish()
+        }
+    }
+}
