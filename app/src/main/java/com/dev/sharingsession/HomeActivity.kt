bd067d360baf5eaf9b4c27484d74460d287857bd
Muhammad Arinal <arinal.rnl@gmail.com>
2019-06-20 18:12:49 +0700

    Revert "sharing-190620"
    This reverts commit c7f2636298456aeb7edd91e496022821bdc03b79.

--- app/src/main/java/com/dev/sharingsession/HomeActivity.kt
+++ app/src/main/java/com/dev/sharingsession/HomeActivity.kt
@@ -1,9 +1,8 @@
-package com.dev.sharingsession.april25
+package com.dev.sharingsession
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.dev.sharingsession.R
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
--- app/src/main/java/com/dev/sharingsession/juni20/view/MainContract.kt
+++
@@ -1,8 +0,0 @@
-package com.dev.sharingsession.juni20.view
-
-import com.dev.sharingsession.juni20.model.UserData
-
-interface MainContract {
-    fun hitApiOneUSer()
-    fun dialogShow(user: UserData)
-}
